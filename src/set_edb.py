d9c65368f577bbc888245ea74be11d056ce94c15
mlin <mingchih.lin8@gmail.com>
2025-10-12 11:34:48 +0800

    set_edb.py ready

---
+++ src/set_edb.py
@@ -0,0 +1,34 @@
+import sys
+from pyaedt import Edb
+import json
+
+json_path = sys.argv[1]
+# json_path = '../data/ports.json'
+
+with open(json_path) as f:
+    info = json.load(f)
+
+
+edb = Edb(info['aedb_path'], edbversion='2024.1')
+new_edb_path = info['aedb_path'].replace('.aedb', '_applied.aedb')
+
+
+ref_terminals = {}
+for comp_name in info['controller_components'] + info['dram_components']:
+    net_name = info['reference_net']
+    pgname, pg = edb.siwave.create_pin_group_on_net(comp_name, net_name, f'{comp_name}_{net_name}_ref')
+    ref_terminals[comp_name] = pg.create_port_terminal(50)
+    
+#%%
+for port in info['ports']:
+    port_name = port['name']
+    comp_name = port['component']
+    net_name = port['net']
+
+    pgname, pg = edb.siwave.create_pin_group_on_net(comp_name, net_name,  f'{comp_name}_{net_name}')
+    terminal = pg.create_port_terminal(50)
+    terminal.SetReferenceTerminal(ref_terminals[comp_name])
+    terminal.SetName(port_name)
+
+edb.save_edb_as(new_edb_path)
+edb.close_edb()
